f7062a5a78e94085544378b0f7f8cc5e6c19d600
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2026-04-08 13:08:16 +0900

    1-01-exp

--- 1-01-exp/src/main.cpp
+++ 1-01-exp/src/main.cpp
@@ -1,7 +1,12 @@
+// Arduino 1-01-ex1
+// CreatedAt: 2026-04-08
+// (C) 2026 Michiru Tsurumaru; MIT LICENSE
+
 #include <M5Core2.h>
 
 void setup() {
     M5.begin();
+    M5.Lcd.setTextSize(3);
     M5.Lcd.print("Hello World");
 }
 
